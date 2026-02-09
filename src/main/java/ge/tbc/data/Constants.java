e3e6b85ef6bb736bdf591413f2616974effafd04
Mjabanashvili <megi_jabanashvili@yahoo.com>
2026-02-09 09:53:18 +0400

    add folders

---
+++ src/main/java/ge/tbc/data/Constants.java
@@ -0,0 +1,5 @@
+package ge.tbc.data;
+
+public class Constants {
+public static final String BASE_URL = "https://demoqa.com/automation-practice-form";
+}
---
+++ src/test/java/baseTests/BaseTest.java
@@ -0,0 +1,47 @@
+package baseTests;
+
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.WaitUntilState;
+import ge.tbc.data.Constants;
+import org.testng.annotations.BeforeClass;
+
+import java.util.List;
+
+public class BaseTest {
+    //ესენი იმისთვის რომ გაიხსნას გვერდი
+    protected Playwright playwright;
+    protected Browser browser;
+    protected BrowserContext context;
+    protected Page page;
+
+    //ეს ნაწილი უნდა გაეშვას ჩვენი ტესტის დაწყებამდე
+    @BeforeClass
+    //ამ მეთოდში არის ის რაც გვჭირდება გვერდის გასახსნელად
+    public void setUp() {
+        playwright = Playwright.create();
+
+        browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions()
+                        .setHeadless(false)
+                        .setArgs(List.of("--start-maximized"))
+        );
+
+        context = browser.newContext(
+                new Browser.NewContextOptions().setViewportSize(null)
+        );
+
+        page = context.newPage();
+        page.navigate(
+                Constants.BASE_URL,
+                new Page.NavigateOptions()
+                        .setTimeout(60_000)    //აქ ტესტი ელოდება 60 წმ გვერდის ჩატვირთვას ,რო გაეშვას
+                        .setWaitUntil(WaitUntilState.LOAD)  //სანამ გვერდი ბოლომდე არ ჩაიტვირთება მანამდე დაელოდება
+        );
+
+    }
+    //ეს ფუნქცია მიახვედრებს რომელ გვერძე ხდება ეს ყველაფერი, ტესტის გაშვებისას მე რომ სხვა გვერძე გადავიდე,
+    //ეს ჩემმა ტესტმა მაინც იქ უნდა გააგრძელოს გატესტვა სადაც მივუთითებ და ამას აკეთებს ეს ფუნქცია
+    protected Page getPage() {
+        return page;
+    }
+}
